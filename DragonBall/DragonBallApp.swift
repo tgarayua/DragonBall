fcf6b487ff083cf60cc466382475195d513d5c91
Thomas Garayua <tgarayua@gmail.com>
2024-11-13 13:40:50 -0500

    first commit

---
+++ DragonBall/DragonBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallApp.swift
+//  DragonBall
+//
+//  Created by Thomas Garayua on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
